04582b44c3966c24428af7e704afef52156809be
Michael Su <msu242979@gmail.com>
2017-04-19 17:48:50 -0400

    fh

--- myStuff.js
+++ myStuff.js
@@ -1,7 +1,7 @@
 var app = angular.module("myApp", ["ngMaterial","jkAngularCarousel"]);
 app.controller("myController", function($scope){
 	$scope.arrayData = [
-		{src: "1.jpg"},
+		{src: "https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg},
 		{src: "2.jpg"},
 		{src: "3.jpg"},
 		{src: "4.jpg"},
