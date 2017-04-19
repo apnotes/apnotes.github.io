4f57196378803301e9ba590b0fb1c640e5b99832
Michael Su <msu242979@gmail.com>
2017-04-19 17:17:26 -0400

    added carousel

--- myStuff.js
+++ myStuff.js
@@ -1,4 +1,13 @@
 var app = angular.module("myApp", ["ngMaterial","jkAngularCarousel"]);
+app.controller("myController", function($scope){
+	$scope.arrayData = [
+		{src: "1.jpg"},
+		{src: "2.jpg"},
+		{src: "3.jpg"},
+		{src: "4.jpg"},
+		{src: "5.jpg"}
+	]
+});
 app.run(function (){
 	console.log("THis is running");
 });
